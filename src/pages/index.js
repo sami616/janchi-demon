a3dbc61ce0ea27159247f4dabf22d7d3d7e56592
sami616 <samuelresua@googlemail.com>
2019-01-08 19:45:50 +0000

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,31 +3,31 @@ import { graphql, Link } from 'gatsby'
 
 // COMPONENT
 const App = ({ data }) => {
-  const { edges: pages } = data.allContentfulPage
-  return pages.map(page => {
-    const { id, title, slug } = page.node
-    return (
-      <div key={id}>
-        <h1>{title}</h1>
-        <Link to={slug}>Read more</Link>
-      </div>
-    )
-  })
+	const { edges: pages } = data.allContentfulPage
+	return pages.map(page => {
+		const { id, title, slug } = page.node
+		return (
+			<div key={id}>
+				<h1>{title}</h1>
+				<Link to={slug}>Read more</Link>
+			</div>
+		)
+	})
 }
 
 export default App
 
 // QUERY
 export const query = graphql`
-  query getPages {
-    allContentfulPage(sort: { fields: [createdAt], order: DESC }) {
-      edges {
-        node {
-          id
-          title
-          slug
-        }
-      }
-    }
-  }
+	query getPages {
+		allContentfulPage(sort: { fields: [createdAt], order: DESC }) {
+			edges {
+				node {
+					id
+					title
+					slug
+				}
+			}
+		}
+	}
 `
--- src/templates/page.js
+++ src/templates/page.js
@@ -4,61 +4,57 @@ import parser from 'html-react-parser'
 
 // COMPONENT
 const Page = ({ data }) => {
-  const { contentfulPage: page } = data
-  const { id, title, excerpt, body, createdAt, featuredImage, author } = page
-  const { file, title: imgTitle, description } = featuredImage
+	const { contentfulPage: page } = data
+	const { id, title, excerpt, body, createdAt, featuredImage, author } = page
+	const { file, title: imgTitle, description } = featuredImage
+	const {
+		childMarkdownRemark: { html: excerptHTML }
+	} = excerpt
+	const {
+		childMarkdownRemark: { html: bodyHTML }
+	} = body
 
-  const {
-    childMarkdownRemark: { html: excerptHTML }
-  } = excerpt
-
-  const {
-    childMarkdownRemark: { html: bodyHTML }
-  } = body
-
-  return (
-    <div key={id}>
-      <h1>{title}</h1>
-      <time>{createdAt}</time>
-      <hr />
-      {parser(excerptHTML)}
-      <hr />
-      {parser(bodyHTML)}
-      <img style={{ width: '100%' }} title={imgTitle} alt={description} src={file.url} />
-      {author && <p>Post By {author.name}</p>}
-    </div>
-  )
+	return (
+		<div key={id}>
+			<h1>{title}</h1>
+			<time>{createdAt}</time>
+			{parser(excerptHTML)}
+			{parser(bodyHTML)}
+			<img style={{ width: '100%' }} title={imgTitle} alt={description} src={file.url} />
+			{author && <p>Post By {author.name}</p>}
+		</div>
+	)
 }
 
 export default Page
 
 // QUERY
 export const pageQuery = graphql`
-  query pageQuery($id: String!) {
-    contentfulPage(id: { eq: $id }) {
-      id
-      title
-      createdAt(formatString: "MMMM DD, YYYY")
-      featuredImage {
-        title
-        description
-        file {
-          url
-        }
-      }
-      author {
-        name
-      }
-      excerpt {
-        childMarkdownRemark {
-          html
-        }
-      }
-      body {
-        childMarkdownRemark {
-          html
-        }
-      }
-    }
-  }
+	query pageQuery($id: String!) {
+		contentfulPage(id: { eq: $id }) {
+			id
+			title
+			createdAt(formatString: "MMMM DD, YYYY")
+			featuredImage {
+				title
+				description
+				file {
+					url
+				}
+			}
+			author {
+				name
+			}
+			excerpt {
+				childMarkdownRemark {
+					html
+				}
+			}
+			body {
+				childMarkdownRemark {
+					html
+				}
+			}
+		}
+	}
 `
